108a43d5f5915f050e2023c0f9dc66d0cc950c59
kimdp5959 <kimdp5959@gmail.com>
2024-01-02 14:57:46 +0900

    변경: 컨트롤러 수정 & API경로 추가
    * 소스변경
    * API 경로 추가

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -1,11 +1,26 @@
 <?php
 
 namespace App\Http\Controllers\Api;
-
-use App\Http\Controllers\Controller;
+//Post 모델 선언
+use App\Models\Post;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+//API 리소스 선언
+use App\Http\Resources\PostResource;
 
 class PostController extends Controller
 {
-	//
+	/**
+	 * 기본화면(리스트)
+	 *
+	 * @return void
+	 */
+	public function index()
+	{
+		//posts 전체 가져오기
+		$posts = Post::latest()->paginate(5);
+
+		//API 리소스에 결과값 리턴
+		return new PostResource(true, 'List Data Posts', $posts);
+	}
 }
