9a9399d12f1b6800f7ec8bc5c519be08a511b240
kimdp5959 <kimdp5959@gmail.com>
2024-01-02 15:05:15 +0900

    추가: 데이터 등록
    * 등록 메소스(store) 추가

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -7,6 +7,8 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 //API 리소스 선언
 use App\Http\Resources\PostResource;
+//Facade 폼검증 선언
+use Illuminate\Support\Facades\Validator;
 
 class PostController extends Controller
 {
@@ -22,5 +24,40 @@ class PostController extends Controller
 
 		//API 리소스에 결과값 리턴
 		return new PostResource(true, 'List Data Posts', $posts);
+	}
+
+	/**
+	 * 등록
+	 *
+	 * @param  mixed $request
+	 * @return void
+	 */
+	public function store(Request $request)
+	{
+		//폼 검증 유효성 규칙 선언
+		$validator = Validator::make($request->all(), [
+			'image'     => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+			'title'     => 'required',
+			'content'   => 'required',
+		]);
+
+		//폼 검증 유효성 오류시 처리
+		if ($validator->fails()) {
+			return response()->json($validator->errors(), 422);
+		}
+
+		//이미지 업로드
+		$image = $request->file('image');
+		$image->storeAs('public/posts', $image->hashName());
+
+		//글 작성
+		$post = Post::create([
+			'image'     => $image->hashName(),
+			'title'     => $request->title,
+			'content'   => $request->content,
+		]);
+
+		//처리 응답값 선언
+		return new PostResource(true, 'Data Post Berhasil Ditambahkan!', $post);
 	}
 }
