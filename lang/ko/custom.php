a3918de4d24118bea0dc07ed1d1cfdc1855ba24f
kimdp5959 <kimdp5959@gmail.com>
2024-01-03 14:20:38 +0900

    추가: 언어 지역화 구성 및 설정
    * 사용자 지정 공통 언어 파일(lang/ko/custom.php) 추가

---
+++ lang/ko/custom.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    // 공통 메세지 관련
+    'cm_have_data_success' => ':title 데이터 조회에 성공하었습니다.',
+    'cm_have_no_data_error'   => ':title 데이터가 존재하지 않습니다.',
+    // 공통 CRUD 관련
+    'cm_create_success' => ':title 생성이 정상 처리되었습니다.',
+    'cm_update_success' => ':title 정보를 수정하였습니다.',
+    'cm_put_success' => ':title 정보를 수정하였습니다.',
+    'cm_delete_success' => ':title 삭제를 처리하였습니다.',
+    'cm_create_error' => ':title 생성에 실패했습니다.',
+    'cm_update_error' => ':title 정보 수정에 실패했습니다.',
+    'cm_put_error' => ':title 정보 수정에 실패했습니다.',
+    'cm_delete_error' => ':title 삭제 처리에 실패했습니다.',
+    'cm_search_error' => ':title 검색에 실패했습니다.',
+    'cm_deleteall_error' => ':title 전체 삭제에 실패했습니다.',
+    'cm_exist_success' => '존재하는 :title 입니다.',
+    'cm_exist_error' => '존재하지 않는 :title입니다.',
+];
